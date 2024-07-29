2e89a83c30aa4ac3c03b903d178c79a85a3e2846
Arshad Qadri <arshad.qadri@caprium.com>
2024-07-29 17:12:41 +0530

    changes

--- src/controller/generatepdf.js
+++ src/controller/generatepdf.js
@@ -1,20 +1,24 @@
 const puppeteer = require('puppeteer');
 
 exports.generatePdf = async (req, res) => {
-  const htmlContent = `<html><body><h1>My PDF Content</h1>
-  <img height="200" src="https://images.pexels.com/photos/24460824/pexels-photo-24460824/free-photo-of-esb-among-lower-skyscrapers.jpeg" alt="test" />
-  <p>This is a sample PDF generated from HTML.</p></body></html>`;
+  const htmlContent = `
+    <html>
+      <body>
+        <h1>My PDF Content</h1>
+        <img height="200" src="https://images.pexels.com/photos/24460824/pexels-photo-24460824/free-photo-of-esb-among-lower-skyscrapers.jpeg" alt="test" />
+        <p>This is a sample PDF generated from HTML.</p>
+      </body>
+    </html>
+  `;
 
   try {
-    // Ensure Puppeteer has the necessary Chromium installed
-    await puppeteer.launch(); // This will automatically download Chromium if not available
-
     // Launch Puppeteer with necessary arguments
     const browser = await puppeteer.launch({
       headless: true,
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      executablePath: puppeteer.executablePath() // Ensure Puppeteer uses the correct executable path
+      executablePath: puppeteer.executablePath(), // Ensure Puppeteer uses the correct executable path
     });
+
     const page = await browser.newPage();
 
     // Set the HTML content
